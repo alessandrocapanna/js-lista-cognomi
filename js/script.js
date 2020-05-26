83e713c927fddbfe0fb32cc67632a134abb781a7
alessandrocapanna <acapanna75@gmail.com>
2020-05-26 19:08:40 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -10,4 +10,4 @@ cognomiPresenti.sort();
 alert('questa è la lista dei cognomi in ordine alfabeetico: ' + cognomiPresenti);
 
 // scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
-alert('la posizione umana è: ' + cognomiPresenti.indexOf(cognomeUtente) + 1);
+alert('la posizione umana è: ' + (cognomiPresenti.indexOf(cognomeUtente) + 1)+ ' su ' + (cognomiPresenti.length));
