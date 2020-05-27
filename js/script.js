1adbdf3e8c61c23258bfe92498cc156851b4ed33
alessandrocapanna <acapanna75@gmail.com>
2020-05-27 11:12:44 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 var cognomeUtente = prompt("inserisci il tuo cognome");
 var cognomiPresenti = [ 'bianchi', 'rossi', 'duzioni', 'balsano', 'verdi'];
 var elementoLista = document.getElementById('lista');
+var posizione = document.getElementById('posizione');
 
 // verifico se il cognome è vuoto ,se è un numero ,inserisco nuovo cognome e ordino alfabeeticamente
 while ( (isNaN(parseInt(cognomeUtente)) === false) || cognomeUtente.length == 0) {
@@ -15,4 +16,4 @@ for (var i = 0; i < cognomiPresenti.length; i++) {
   elementoLista.innerHTML += '<li>' + cognomiPresenti[i] + '</li>';
   // elementoLista.innerHTML = elementoLista.innerHTML + '<li>' + cognomiPresenti[i] + '</li>';
 }
-console.log('la posizione umana è: ' + (cognomiPresenti.indexOf(cognomeUtente) + 1)+ ' su ' + (cognomiPresenti.length));
+posizione.innerHTML = ('la posizione umana è: ' + (cognomiPresenti.indexOf(cognomeUtente) + 1)+ ' su ' + (cognomiPresenti.length));
